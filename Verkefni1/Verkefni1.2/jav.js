d112255f6fed01f3ae7078b012213984fe7db77a
Pálmi C Rúnarsson <pancakeprins@gmail.com>
2017-08-24 21:43:19 +0000

    Verkfni

---
+++ Verkefni1/Verkefni1.2/jav.js
@@ -0,0 +1,12 @@
+var lenght;
+var hight;
+var breidd;
+var rummal;
+
+lenght = 6;
+hight = 8;
+breidd = 5;
+rummal = lenght * hight * breidd;
+
+var rum = document.getElementById('rumm');
+rum.textContent = 'Rúmmal> ' + rummal;
---
+++ Verkefni1/Verkefni1.3/jav2.js
@@ -0,0 +1,8 @@
+let list = ["Apple", "Bannana", "Milk", "Salt", "Bleach"]
+let text = "";
+for (let i = 0; i < list.length; i++) {
+	text += list[i] + "<br>";
+}
+document.getElementById('storelist').innerHTML = text;
+
+document.write(i);
